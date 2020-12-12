28f124c8c68b143ea614f74532ad85c2043e9b66
Abhijith Mundanad Narayanan <mabhijithn@gmail.com>
2020-12-12 15:22:33 +0100

    Updated figures, added tables of goals, more options of y-axis

--- index.py
+++ index.py
@@ -34,8 +34,8 @@ descriptionTab1 = dcc.Markdown('''
 **Description of axis variables**
 
 * **Goals per 90 mins**:  (total goals/total minutes played) X 90
-* **Points won (normalised)**: If a player's goal was the final goal and that was the game-defining goal, i.e. leading to a draw (+1 point) or a win (+3 points). 
-   * **y-axis**: = total points won/(3*matches played)
+* **Points won**: If a player's goal was the final goal and that was the game-defining goal, i.e. leading to a draw (+1 point) or a win (+3 points). 
+   * **y-axis**: = total points won
 * **Size** of the blobs: Total goals
 ''')
 
@@ -78,12 +78,20 @@ app.layout = html.Div([
     ]),
     html.Br(),
     dcc.Tabs([
-        dcc.Tab(label='Points Won for Team',selected_style=tab_selected_style,
+        dcc.Tab(label='Points Won for Team',selected_style=tab_selected_style,            
             children=[
+            html.Br(),
+            dcc.RadioItems(id='radio-pointsaxis',
+                options = [
+                 {'label':'(y-axis) Points Won','value':'PointsWon'},
+                 {'label':'(y-axis) Points won normalised by matches played','value':'PointsNorm'}                
+                ],
+                value = 'PointsWon'
+                ),
             dcc.Graph(id='gper90-v-ptsnorm-graph'),
             descriptionTab1,
             dcc.Dropdown(id='players-dropdown'),
-            dash_table.DataTable(id='player-goals',
+            dash_table.DataTable(id='points-goals',
             style_cell={'textAlign': 'left'},
             style_data_conditional=[
             {
@@ -97,8 +105,27 @@ app.layout = html.Div([
             ]),
         dcc.Tab(label='Game Changing Goals for Team',selected_style=tab_selected_style,
             children=[
+            dcc.RadioItems(id='radio-gcgoalaxis',
+                options = [                 
+                 {'label':'(y-axis) Game-changing goals (normalised)','value':'GCgoalsPapp'},
+                 {'label':'(y-axis) Game-changing goals','value':'GCGoals'}
+                ],
+                value = 'GCgoalsPapp'
+                ),
             dcc.Graph(id='gdper90-v-gcgoalspct-graph'),
-            descriptionTab2
+            descriptionTab2,
+            dcc.Dropdown(id='players-dropdown-2'),
+            dash_table.DataTable(id='gc-goals',
+            style_cell={'textAlign': 'left'},
+            style_data_conditional=[
+            {
+                'if': {
+                    'filter_query': '{GCGoal} eq "Yes"',
+                    },
+                'backgroundColor': '#FF4136',
+                'color': 'white'
+            },
+            ])
             ])
         ]),
     ])
@@ -127,91 +154,125 @@ def get_goaldata(clubname):
 @app.callback(
     Output('gper90-v-ptsnorm-graph', 'figure'),
     Output('gdper90-v-gcgoalspct-graph','figure'),
-    Output('slider-display','children'),
+    Output('slider-display','children'),    
     Input('min-goal-slider','value'),
-    Input('club-display','children')
+    Input('club-display','children'),
+    Input('radio-pointsaxis','value'),
+    Input('radio-gcgoalaxis','value')
     )
-def update_plot(minGoals,clubselected):
+def update_plot(minGoals,clubselected,pointsaxis,gcgoalsaxis):
     displaystr = f'Minimum Goals: {minGoals}'
     goalScorersAll = pd.read_csv('allEPLgoalScorers.csv')
-
+    playerstatsAll = pd.read_csv('epl_aggregate_appearances_with_goals_allplayers_2000_2019.csv')
     
     if 'All Clubs' not in clubselected:
         goalScorers = goalScorersAll[goalScorersAll['Club'].str.contains(clubselected)]
+        playerstats = playerstatsAll[playerstatsAll['Club']==clubselected]
     else:
         goalScorers = goalScorersAll
-    goalScorers = goalScorers[goalScorers['Goals']>=minGoals]
-    goalScorers.sort_values(by='Goals',ascending=False,inplace=True)
-    goalScorers.loc[:,'Goals'] = pd.to_numeric(goalScorers.loc[:,'Goals'])
-    goalScorers.loc[:,'GCgoals'] = pd.to_numeric(goalScorers.loc[:,'GCgoals'])
-    goalScorers.loc[:,'Appearances'] = pd.to_numeric(goalScorers.loc[:,'Appearances'])
-    goalScorers.loc[:,'Minutes'] = pd.to_numeric(goalScorers.loc[:,'Minutes'])
-    goalScorers.loc[:,'GoalsPer90'] = 90*(goalScorers.loc[:,'Goals']/goalScorers.loc[:,'Minutes'])
-    goalScorers.loc[:,'PointsNorm'] = goalScorers.loc[:,'Points']/(3*goalScorers.loc[:,'Appearances'])
-    goalScorers.loc[:,'GCgoalsPCT'] = goalScorers.loc[:,'GCgoals']/goalScorers.loc[:,'Goals']    
-    goalScorers.loc[:,'GCgoalsPapp'] = goalScorers.loc[:,'GCgoals']/goalScorers.loc[:,'Appearances']    
+        playerstats = playerstatsAll
+        
+    datatoplot = playerstats[playerstats['Goals']>=minGoals]
+    datatoplot.sort_values(by='Goals',ascending=False,inplace=True)
+    datatoplot.loc[:,'Goals'] = pd.to_numeric(datatoplot.loc[:,'Goals'])
+    datatoplot.loc[:,'GCGoals'] = pd.to_numeric(datatoplot.loc[:,'GCGoals'])
+    datatoplot.loc[:,'Appearances'] = pd.to_numeric(datatoplot.loc[:,'Appearances'])
+    datatoplot.loc[:,'Minutes'] = pd.to_numeric(datatoplot.loc[:,'Mins'])
+    datatoplot.loc[:,'GoalsPer90'] = 90*(datatoplot.loc[:,'Goals']/datatoplot.loc[:,'Mins'])
+    datatoplot.loc[:,'PointsNorm'] = datatoplot.loc[:,'PointsWon']/(datatoplot.loc[:,'Appearances'])
+    datatoplot.loc[:,'GCgoalsPCT'] = datatoplot.loc[:,'GCGoals']/datatoplot.loc[:,'Goals']    
+    datatoplot.loc[:,'GCgoalsPapp'] = datatoplot.loc[:,'GCGoals']/datatoplot.loc[:,'Appearances'] 
+    playerstatsAll.loc[:,'PointsNorm'] = playerstatsAll.loc[:,'PointsWon']/(3*playerstatsAll.loc[:,'Appearances'])
+    playerstatsAll.loc[:,'GoalsPer90'] = 90*(playerstatsAll.loc[:,'Goals']/playerstatsAll.loc[:,'Mins'])
+    gsDF = playerstatsAll[playerstatsAll['Goals']>=minGoals]    
     
-    goalScorersAll.loc[:,'PointsNorm'] = goalScorersAll.loc[:,'Points']/(3*goalScorersAll.loc[:,'Appearances'])
-    goalScorersAll.loc[:,'GoalsPer90'] = 90*(goalScorersAll.loc[:,'Goals']/goalScorersAll.loc[:,'Minutes'])
-    gsDF = goalScorersAll[goalScorersAll['Goals']>=minGoals]
-    maxptsall = gsDF['PointsNorm'].max()
+    if 'PointsWon' in pointsaxis:
+        maxptsall= datatoplot['PointsWon'].max()
+        ylabel = 'Points Won'
+    else:
+        maxptsall = datatoplot['PointsNorm'].max()    
+        ylabel = 'Points Won (Normalised)'
+    
     maxgp90all = gsDF['GoalsPer90'].max()
     maxgoalsAll = gsDF['Goals'].max()
-    maxGCgoalsAll = gsDF['GCgoals'].max()
+    maxGCgoalsAll = gsDF['GCGoals'].max()
     
-    maxpts = goalScorers['PointsNorm'].max()    
-    maxgp90 = goalScorers['GoalsPer90'].max()
-    maxapps = goalScorers['Appearances'].max()
-    maxgcPapp = goalScorers['GCgoalsPapp'].max()
-    
+    maxpts = datatoplot['PointsNorm'].max()    
+    maxgp90 = datatoplot['GoalsPer90'].max()
+    maxapps = datatoplot['Appearances'].max()
+    maxgcPapp = datatoplot['GCgoalsPapp'].max()
+    goalScorers = datatoplot
+
     x_range = np.arange(0,maxgp90+0.1,0.05)
     x_range = x_range.tolist()
     y_range_max = maxptsall*np.ones(len(x_range))
     y_range_max = y_range_max.tolist()
+    
+    if 'PointsWon' in pointsaxis:
+        y_range = [-1,maxptsall+5]
+        y_dtick = 5
+    else:
+        y_range = [-0.1,maxptsall+0.1]
+        y_dtick = 0.05
+    
+    if 'GCgoalsPapp' in gcgoalsaxis:
+        maxgcPapp = datatoplot['GCgoalsPapp'].max()
+        y_range_gc = [0,maxgcPapp+0.1]
+        y_dtick_gc = 0.1
+    else:
+        maxGCgoals = datatoplot['GCGoals'].max()
+        y_range_gc = [-1,maxGCgoals+5]
+        y_dtick_gc = 5    
         
     if 'All Clubs' not in clubselected:        
-        fig = px.scatter(goalScorers,x='GoalsPer90',y='PointsNorm',color='Name',
+        fig = px.scatter(goalScorers,x='GoalsPer90',y=pointsaxis,color='Name',hover_name='Name',
                      size='Goals',size_max=40)
-        fig1 = px.scatter(goalScorers,x='GoalsPer90',y='GCgoalsPapp',color='Name',
+        fig1 = px.scatter(goalScorers,x='GoalsPer90',y=gcgoalsaxis,color='Name',hover_name='Name',
                      size='Goals',size_max=40)
     else:
-        fig = px.scatter(goalScorers,x='GoalsPer90',y='PointsNorm',color='Club',hover_name='Name',
+        fig = px.scatter(goalScorers,x='GoalsPer90',y=pointsaxis,color='Club',hover_name='Name',
                      size='Goals',size_max=40) 
-        fig1 = px.scatter(goalScorers,x='GoalsPer90',y='GCgoalsPapp',color='Club',hover_name='Name',
+        fig1 = px.scatter(goalScorers,x='GoalsPer90',y=gcgoalsaxis,color='Club',hover_name='Name',
                      size='Goals',size_max=40)
     
-    fig.update_layout(xaxis_title='Goals per 90 mins',yaxis_title='Points won for team (normalised)',
+    fig.update_layout(xaxis_title='Goals per 90 mins',yaxis_title=ylabel,
                       xaxis=dict(range=[0,maxgp90all+0.1],tickmode='linear',tick0=0,dtick=0.1),
-                      yaxis=dict(range=[-0.1,maxptsall+0.1],tickmode='linear',tick0 = 0,dtick=0.1))
+                      yaxis=dict(range=y_range,tickmode='linear',tick0 = 0,dtick=y_dtick))
     fig1.update_layout(xaxis_title='Goals per 90 mins',yaxis_title='Game-Changing Goals per Appearance',
                       xaxis=dict(range=[0,maxgp90+0.1],tickmode='linear',tick0=0,dtick=0.1),
-                       yaxis=dict(range=[0,maxgcPapp+0.1],tickmode='linear',tick0 = 0,dtick=0.1))
+                       yaxis=dict(range=y_range_gc,tickmode='linear',tick0 = 0,dtick=y_dtick_gc))
     return fig,fig1,displaystr
 
 @app.callback(
     Output('players-dropdown','options'),
     Output('players-dropdown','value'),
+    Output('players-dropdown-2','options'),
+    Output('players-dropdown-2','value'),
     Input('min-goal-slider','value'),
     Input('clubname-dropdown','value')
     )
-def playerdropdown(minGoals,clubselected):
-    goalScorersAll = pd.read_csv('allEPLgoalScorers.csv')
-
+def playerdropdown(minGoals,clubselected):    
+    playerstatsAll = pd.read_csv('epl_aggregate_appearances_with_goals_allplayers_2000_2019.csv')
     
-    if 'All Clubs' not in clubselected:
-        goalScorers = goalScorersAll[goalScorersAll['Club'].str.contains(clubselected)]
+    if 'All Clubs' not in clubselected:        
+        playerstats = playerstatsAll[playerstatsAll['Club']==clubselected]
     else:
-        goalScorers = goalScorersAll
-    goalScorers = goalScorers[goalScorers['Goals']>=minGoals]
+        playerstats = playerstatsAll
+
+    playerstats = playerstats[playerstats['Goals']>=minGoals]
+    playerstats = playerstats.sort_values(by='Goals',ascending=False)
     
-    playernames = goalScorers['Name'].tolist()
+    playernames = playerstats['Name'].tolist()
     options_ret = [{'label':i, 'value':i} for i in playernames]
     value_ret = playernames[0]
-    return options_ret,value_ret
+    
+    options_ret2 = options_ret
+    value_ret2 = value_ret
+    return options_ret,value_ret,options_ret2,value_ret2
 
 @app.callback(
-    Output('player-goals','columns'),
-    Output('player-goals','data'),
+    Output('points-goals','columns'),
+    Output('points-goals','data'),
     Input('players-dropdown','value'),
     Input('clubname-dropdown','value')
     )
@@ -227,6 +288,33 @@ def update_table(playername,clubselected):
     
     coulumnlist = ['Date','Season','TeamFor','TeamAga','Venue','Time','Score','FTScore','PointsWon']
     columnnames = ['Date','Season','For','Against','Venue','Time','Score','Full Time Score','Points Won']
+    columns=[{"name": columnnames[i], "id": coulumnlist[i]} for i in range(len(coulumnlist))]
+    playergoalsdict = playergoals.to_dict('records')
+    return columns,playergoalsdict
+
+@app.callback(
+    Output('gc-goals','columns'),
+    Output('gc-goals','data'),
+    Input('players-dropdown-2','value'),
+    Input('clubname-dropdown','value')
+    )
+def update_table2(playername,clubselected):
+    goaleventsDB = pd.read_csv('epl_goalevents_2000_2019.csv')
+    if 'All Clubs' not in clubselected:
+        playeridx = (goaleventsDB['Player']==playername) & (goaleventsDB['TeamFor']==clubselected)
+    else:
+        playeridx = (goaleventsDB['Player']==playername)
+    playergoals = goaleventsDB[playeridx]
+    playergoals.loc[:,'Date'] = pd.to_datetime(playergoals.loc[:,'Date'])
+    playergoals.loc[:,'Date'] = playergoals.loc[:,'Date'].dt.strftime('%A, %d. %B %Y')    
+    
+    gcgoalyes = playergoals['GCGoal']==True
+    gcgoalno = playergoals['GCGoal']==False
+    playergoals.loc[gcgoalyes,'GCGoal'] = 'Yes'
+    playergoals.loc[gcgoalno,'GCGoal'] = 'No'
+    
+    coulumnlist = ['Date','Season','TeamFor','TeamAga','Venue','Time','Score','FTScore','GCGoal']
+    columnnames = ['Date','Season','For','Against','Venue','Time','Score','Full Time Score','Game Changing Goal']
     columns=[{"name": columnnames[i], "id": coulumnlist[i]} for i in range(len(coulumnlist))]
     playergoalsdict = playergoals.to_dict('records')
     return columns,playergoalsdict
